e364dae0ff3fdca67875026ce9e0968def49dc38
Eric Fernandes Pereira <83287307+EricFernandes26@users.noreply.github.com>
2024-04-19 17:54:04 -0300

    exe.bat

---
+++ exe.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem Executa o script Python comparator.py
+"C:\Users\Administrator\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\Administrator\Desktop\ok\comparator.py"
+
+
+echo Scripts Python executado com sucesso!
